1180f8b753850b03423cbc79dada72a88eee7d3d
jimmy <vphu456@gmail.com>
2022-07-29 16:44:33 +0700

    change

---
+++ food-dashboard-template/food-dashboard/app/models/constant/constant.js
@@ -0,0 +1,13 @@
+export const LOAI_MON = {
+  "": "chọn loại món",
+  loai1: "Chay",
+  loai2: "Mặn",
+};
+
+export const TINH_TRANG = {
+  "": "chọn tình trạng",
+  0: "Hết",
+  1: "Còn",
+};
+// loaiMon.loai1 => chay
+// loaiMon["loai1"] => chay
